cd183ecb3517ed1d1c62cb382870774640bb0cc6
zhangjie <jzhang00@163.com>
2020-10-17 14:49:28 +0800

    fix cannot get environment bug

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,8 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+    NODE_ENV: '"development"',
+    API_BASE_URL: JSON.stringify(process.env.API_BASE_URL),
+    AUTHORITY_URL: JSON.stringify(process.env.AUTHORITY_URL),
+    CLIENT_URL: JSON.stringify(process.env.CLIENT_URL)
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,7 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+    NODE_ENV: '"production"',
+    API_BASE_URL: JSON.stringify(process.env.API_BASE_URL),
+    AUTHORITY_URL: JSON.stringify(process.env.AUTHORITY_URL),
+    CLIENT_URL: JSON.stringify(process.env.CLIENT_URL)
 }
