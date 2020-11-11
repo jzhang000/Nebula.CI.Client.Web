c9997bec3a183146817fad256731fec4f2268476
zhangjie <jzhang00@163.com>
2020-11-11 10:06:53 +0800

    modify buildhistory ui

--- src/components/job/buildHistory.vue
+++ src/components/job/buildHistory.vue
@@ -1,105 +1,117 @@
 <template>
-  <div style="height: 100%;width:270px;">
+  <div style="height: 100%; width: 270px">
     <a-config-provider :locale="locale">
-    <a-collapse v-model="activeKey" expand-icon-position="left" style="height:100%">
-      <a-collapse-panel key="1" header="构建历史" style="height:100%">
-        <div class="build-history">
-        <a-list
-          item-layout="vertical"
-          size="small"
-          :pagination="pagination"
-          :data-source="listData"
-        >
-          <a-list-item slot="renderItem" key="item.title" slot-scope="item, index">
-            <div class="buildList">
-              <el-link :underline="false">
-                <img
-                  src="/static/16x16/red.png"
-                  @click="showConsole(item.id)"
-                  title="构建失败"
-                  v-if="item.status=='exception'"
-                />
-                <img
-                  src="/static/16x16/blue.png"
-                  @click="showConsole(item.id)"
-                  title="构建成功"
-                  v-else-if="item.status=='success'"
-                />
-                <img
-                  src="/static/16x16/aborted.png"
-                  @click="showConsole(item.id)"
-                  title="待执行"
-                  v-else-if="item.status=='normal'"
-                />
-                <a-icon type="loading" title="构建中" v-else />
-              </el-link>
-              <el-link
-                @click="showStatus(item.id, item.buildTime)"
-                style="padding-top:3px"
-              >#{{ item.no }}</el-link>
-              <el-link
-                :underline="false"
-                @click="showStatus(item.id, item.buildTime)"
-                style="padding-top:3px;padding-left:10px"
-              >{{ item.buildTime }}</el-link>
-              <a
-                slot="actions"
-                style="float:right;color:red;padding-top:3px"
-                @click="deleteItem(item, index)"
-              >删除</a>
-              <a-progress :percent="item.percent" :status="item.status" />
-            </div>
-          </a-list-item>
-        </a-list>
-        </div>
-        <a-icon slot="extra" type="history" @click="showHistory" />
-      </a-collapse-panel>
-    </a-collapse>
+      <a-collapse
+        v-model="activeKey"
+        expand-icon-position="left"
+        style="height: 100%"
+      >
+        <a-collapse-panel key="1" header="构建历史" style="height: 100%">
+          <div class="build-history">
+            <a-list
+              item-layout="vertical"
+              size="small"
+              :pagination="pagination"
+              :data-source="listData"
+            >
+              <a-list-item
+                slot="renderItem"
+                key="item.title"
+                slot-scope="item, index"
+              >
+                <div class="buildList">
+                  <el-link :underline="false">
+                    <img
+                      src="/static/16x16/red.png"
+                      @click="showConsole(item.id)"
+                      title="构建失败"
+                      v-if="item.status == 'exception'"
+                    />
+                    <img
+                      src="/static/16x16/blue.png"
+                      @click="showConsole(item.id)"
+                      title="构建成功"
+                      v-else-if="item.status == 'success'"
+                    />
+                    <img
+                      src="/static/16x16/aborted.png"
+                      @click="showConsole(item.id)"
+                      title="待执行"
+                      v-else-if="item.status == 'normal'"
+                    />
+                    <a-icon type="loading" title="构建中" v-else />
+                  </el-link>
+                  <el-link
+                    @click="showStatus(item.id, item.buildTime)"
+                    style="padding-top: 3px"
+                    >#{{ item.no }}</el-link
+                  >
+                  <el-link
+                    :underline="false"
+                    @click="showStatus(item.id, item.buildTime)"
+                    style="padding-top: 3px; padding-left: 10px"
+                    >{{ item.buildTime }}</el-link
+                  >
+                  <a
+                    slot="actions"
+                    style="
+                      float: right;
+                      color: red;
+                      padding-top: 3px;
+                      padding-right: 10px;
+                    "
+                    @click="deleteItem(item, index)"
+                    >删除</a
+                  >
+                  <a-progress :percent="item.percent" :status="item.status" />
+                </div>
+              </a-list-item>
+            </a-list>
+          </div>
+          <a-icon slot="extra" type="history" @click="showHistory" />
+        </a-collapse-panel>
+      </a-collapse>
     </a-config-provider>
   </div>
 </template>
 <script>
 import dayjs from "dayjs";
 import api from "@/api";
-import Bus from "@/libs/Bus"
-import zhCN from 'ant-design-vue/es/locale/zh_CN';
+import Bus from "@/libs/Bus";
+import zhCN from "ant-design-vue/es/locale/zh_CN";
 export default {
   data() {
     return {
-      listData : [],
+      listData: [],
       pagination: {
-        onChange: (page) => {
-          
-        },
+        onChange: (page) => {},
         pageSize: 10,
         simple: true,
         hideOnSinglePage: true,
       },
-      activeKey: ['1'],
+      activeKey: ["1"],
       myInterval: null,
-      pipeline : null,
-      locale:zhCN,
-      zhCN
+      pipeline: null,
+      locale: zhCN,
+      zhCN,
     };
   },
   created() {
     var that = this;
-    that.loadHistory()
+    that.loadHistory();
 
-      // setInterval不会清除定时器队列，每重复执行1次都会导致定时器叠加 \
+    // setInterval不会清除定时器队列，每重复执行1次都会导致定时器叠加 \
     that.myInterval = window.setInterval(() => {
-        // setTimeout是自带清除定时器
-        setTimeout(function () {
-          that.loadHistory()
-        }, 600);
-      }, 5000);
+      // setTimeout是自带清除定时器
+      setTimeout(function () {
+        that.loadHistory();
+      }, 600);
+    }, 5000);
   },
   destroyed() {
     clearInterval(this.myInterval);
   },
-  mounted() {
- 
-  },
+  mounted() {},
   methods: {
     showStatus(id, buildTime) {
       this.$router.push({
@@ -114,50 +126,73 @@ export default {
     },
     deleteItem(item, index) {
       //调用api 删除数据库中构建记录
-      let that = this;
-      api.DELETE_PIPELINE_BUILD_API(item.id).then(res => {
-        that.$delete(that.listData, index);
-        Bus.$emit('deleteBuildData', item.id)
+      this.$confirm("确定要删除构建 " + item.no + "？", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+        closeOnClickModal: false,
       })
+        .then(() => {
+          let that = this;
+          api.DELETE_PIPELINE_BUILD_API(item.id).then((res) => {
+            that.$delete(that.listData, index);
+            Bus.$emit("deleteBuildData", item.id);
+          });
+        })
+        .catch(() => {});
     },
     loadHistory() {
-        let that = this;
-        if(that.$route.params.pipelineId){
-          api.PIPELINE_BUILD_HISTORY_API(that.$route.params.pipelineId).then((res) => {
+      let that = this;
+      if (that.$route.params.pipelineId) {
+        api
+          .PIPELINE_BUILD_HISTORY_API(that.$route.params.pipelineId)
+          .then((res) => {
             res.forEach((item, index) => {
-                let pipelineStatus = ""
-                switch (item.status) {
-                    case "running":
-                    pipelineStatus = "active";
-                    break;
+              let pipelineStatus = "";
+              switch (item.status) {
+                case "running":
+                  pipelineStatus = "active";
+                  break;
                 case "Succeeded":
-                    pipelineStatus = "success";
-                    break;
+                  pipelineStatus = "success";
+                  break;
                 case "Failed":
-                    pipelineStatus = "exception"
-                    break
+                  pipelineStatus = "exception";
+                  break;
                 default:
-                    pipelineStatus = "normal"
-                }
-                let runningPipeline = {id: item.id, no : item.no, pipelineId : item.pipelineId, pipelineName: item.pipelineName, percent : item.percent, buildTime : item.startTime, status : pipelineStatus}
-                if(that.listData.length <= index){
-                    that.listData.unshift(runningPipeline)
-                } else {
-                    that.listData[that.listData.length - 1 - index].percent = item.percent
-                    that.listData[that.listData.length - 1 - index].status = pipelineStatus
-                    that.listData[that.listData.length - 1 - index].buildTime = item.startTime 
-                }
-            })
+                  pipelineStatus = "normal";
+              }
+              let runningPipeline = {
+                id: item.id,
+                no: item.no,
+                pipelineId: item.pipelineId,
+                pipelineName: item.pipelineName,
+                percent: item.percent,
+                buildTime: item.startTime,
+                status: pipelineStatus,
+              };
+              if (that.listData.length <= index) {
+                that.listData.unshift(runningPipeline);
+              } else {
+                that.listData[that.listData.length - 1 - index].percent =
+                  item.percent;
+                that.listData[
+                  that.listData.length - 1 - index
+                ].status = pipelineStatus;
+                that.listData[that.listData.length - 1 - index].buildTime =
+                  item.startTime;
+              }
+            });
 
-            Bus.$emit('updateBuildData', this.listData)
+            Bus.$emit("updateBuildData", this.listData);
           });
-        }
-    }
-  }
+      }
+    },
+  },
 };
 </script>
 <style scoped>
-.build-history{
+.build-history {
   display: flex;
   overflow: auto;
   height: 370px;
